f0e7cd1c2adf3f0a1a68c1eea6c062cccf86c513
Donhyeok Kang <dh5473@naver.com>
2025-03-06 23:08:52 +0900

    Fix: port number

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const loadApp = require('./loaders');
 const app = express();
 
 loadApp(app).then(() => {
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.PORT || 5001;
     app.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
     });
